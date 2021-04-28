99750e8c46a4180fa7558017cbbde42568d4f9c8
Craig Baker <cbaker@jpgacademy.org>
2021-04-28 15:33:43 -0500

    added waveform correctly

---
+++ library/waveform-playlist/src/track/states.js
@@ -0,0 +1,17 @@
+import cursor from "./states/CursorState"
+import select from "./states/SelectState"
+import shift from "./states/ShiftState"
+import fadein from "./states/FadeInState"
+import fadeout from "./states/FadeOutState"
+import resizeleft from "./states/ResizeLeftState"
+import resizeright from "./states/ResizeRightState"
+
+export default {
+  cursor,
+  select,
+  shift,
+  fadein,
+  fadeout,
+  resizeleft,
+  resizeright,
+}
