1db6335904b0dc659312e0673520f4b6dff4f683
Craig Baker <cbaker@jpgacademy.org>
2021-05-06 05:47:01 -0500

    Navbar update

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,9 +7,8 @@ export default function NavBar() {
       <Navbar.Brand>DAW Teacher</Navbar.Brand>
       <Nav>
         <Nav.Link href="/page.1">Lesson 1</Nav.Link>
-        <Nav.Link href="/page.2">Lesson 2</Nav.Link>
-
-        <Nav.Link href="/page.3">Lesson 3</Nav.Link>
+        {/* <Nav.Link href="/page.2">Lesson 2</Nav.Link>
+        <Nav.Link href="/page.3">Lesson 3</Nav.Link> */}
       </Nav>
     </Navbar>
   )
