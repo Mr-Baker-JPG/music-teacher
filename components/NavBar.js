05639b620c61efb129a399ffcc5d4365c5819320
Craig Baker <cbaker@jpgacademy.org>
2021-05-06 07:23:05 -0500

    Fixed build errors

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function NavBar() {
   return (
     <Navbar sticky="top" variant="light" bg="light">
-      <Navbar.Brand>DAW Teacher</Navbar.Brand>
+      <Navbar.Brand>Web DAW Teacher</Navbar.Brand>
       <Nav>
         <Nav.Link href="/page.1">Lesson 1</Nav.Link>
         {/* <Nav.Link href="/page.2">Lesson 2</Nav.Link>
