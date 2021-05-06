d26fec5357dfc9b950c0029c2abc4a978e2f76ae
Craig Baker <cbaker@jpgacademy.org>
2021-05-06 07:22:28 -0500

    intro modal formatting

--- components/IntroModal.js
+++ components/IntroModal.js
@@ -11,11 +11,14 @@ export default function HelpModal() {
       <Modal.Header closeButton>
         <Modal.Title>Web DAW Teacher</Modal.Title>
       </Modal.Header>
-      <p>
-        This is my prototype for Web DAW Teacher: a web-based interactive tool to teach the basics of Digital Audio Workstations (DAWs). This sample tutorial page demonstrates the potential for external and teacher-created lessons to teach music history, composition, and DAW aptitude.
-      </p>
       <Modal.Body>
-        
+        <p>
+          This is my prototype for Web DAW Teacher: a web-based interactive tool
+          to teach the basics of Digital Audio Workstations (DAWs). This sample
+          tutorial page demonstrates the potential for external and
+          teacher-created lessons to teach music history, composition, and DAW
+          aptitude.
+        </p>
       </Modal.Body>
     </Modal>
   )
